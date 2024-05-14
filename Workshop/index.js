746e718b55d689c6d71918fe6c195558aff887c4
Nikita S Patil <nikitaspatil311@gmail.com>
2024-05-14 19:49:51 +0200

    add JS,arrays

--- Workshop/index.js
+++ Workshop/index.js
@@ -1,1 +1,14 @@
 console.log("Hello World, Welcome to Web Workshop!!");
+
+let age = 21;
+const name = "Nikita";
+console.log("Age=>", age);
+console.log("Name=>", name);
+
+const arr = ["apple", "Orange", "Mango"];
+console.log("Show me the fruits name=>", arr);
+console.log("First element of an arr=>", arr[0]);
+
+console.log("Second element of an arr=>", arr[1]);
+
+console.log("Third element of an arr=>", arr[2]);
